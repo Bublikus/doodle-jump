a81c832416cc49f41b1358c0fe01bd512cc7abe6
bublik <bublik.script@gmail.com>
2023-12-30 16:40:06 +0200

    Refactor codebase to remove semicolons
    The codebase underwent a minor stylistic overhaul, with all semicolons being removed for a cleaner, more concise code style. All changes are strictly cosmetic and no changes were made to any aspects of the functionality or logic of the code.

--- src/CanvasDoodleRenderer.ts
+++ src/CanvasDoodleRenderer.ts
@@ -1,107 +1,100 @@
-import { RenderData } from "./types";
-import { CanvasImageLoader } from "./CanvasImageLoader";
-import platformImg from "./assets/platform.png";
-import doodleRightImg from "./assets/doodle-right.png";
+import { RenderData } from './types'
+import { CanvasImageLoader } from './CanvasImageLoader'
+import platformImg from './assets/platform.png'
+import doodleRightImg from './assets/doodle-right.png'
 
-const imageLoader = new CanvasImageLoader();
+const imageLoader = new CanvasImageLoader()
 
-const imagesMap = new Map<string, CanvasImageSource>();
+const imagesMap = new Map<string, CanvasImageSource>()
 
 imageLoader.loadMultipleImages([platformImg, doodleRightImg]).then(() => {
-  imagesMap.set(platformImg, imageLoader.getImage(platformImg)!);
-  imagesMap.set(doodleRightImg, imageLoader.getImage(doodleRightImg)!);
-});
+  imagesMap.set(platformImg, imageLoader.getImage(platformImg)!)
+  imagesMap.set(doodleRightImg, imageLoader.getImage(doodleRightImg)!)
+})
 
 export class CanvasDoodleRenderer {
-  private canvas: HTMLCanvasElement;
-  private ctx: CanvasRenderingContext2D;
-  private canvasRatio: number;
-  private resizeObserver: ResizeObserver;
+  private canvas: HTMLCanvasElement
+  private ctx: CanvasRenderingContext2D
+  private canvasRatio: number
+  private resizeObserver: ResizeObserver
 
   constructor(canvas: HTMLCanvasElement) {
-    this.canvas = canvas;
-    this.ctx = canvas.getContext("2d")!;
+    this.canvas = canvas
+    this.ctx = canvas.getContext('2d')!
 
     // Resize the canvas container to adapt the canvas ratio
-    this.canvas.width = canvas.clientWidth;
-    this.canvas.height = canvas.clientHeight;
-    this.canvasRatio = this.canvas.width / this.canvas.height;
-    this.resizeObserver = new ResizeObserver((entries) => {
-      this.canvas.width = entries[0].contentRect.width;
-      this.canvas.height = entries[0].contentRect.height;
-      this.canvasRatio = this.canvas.width / this.canvas.height;
-    });
-    this.resizeObserver.observe(canvas);
+    this.canvas.width = canvas.clientWidth
+    this.canvas.height = canvas.clientHeight
+    this.canvasRatio = this.canvas.width / this.canvas.height
+    this.resizeObserver = new ResizeObserver(entries => {
+      this.canvas.width = entries[0].contentRect.width
+      this.canvas.height = entries[0].contentRect.height
+      this.canvasRatio = this.canvas.width / this.canvas.height
+    })
+    this.resizeObserver.observe(canvas)
   }
 
   private getCanvasX(x: number): number {
-    return x * this.canvas.width;
+    return x * this.canvas.width
   }
 
   private getCanvasY(y: number): number {
-    return y * this.canvas.height;
+    return y * this.canvas.height
   }
 
   public update({ player, platforms, acceleration }: RenderData): void {
     // Clear the canvas
-    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
 
     if (!imagesMap.has(doodleRightImg) || !imagesMap.has(platformImg)) {
-      return;
+      return
     }
 
     // Draw the platforms images
-    platforms.forEach((platform) => {
-      const platformImage = imagesMap.get(platformImg)!;
+    platforms.forEach(platform => {
+      const platformImage = imagesMap.get(platformImg)!
       // @ts-ignore
-      const platformAspectRatio = platformImage.width / platformImage.height;
+      const platformAspectRatio = platformImage.width / platformImage.height
       this.ctx.drawImage(
         platformImage,
         this.getCanvasX(platform.position.x),
         this.getCanvasY(platform.position.y),
         this.getCanvasX(platform.size.width),
-        this.getCanvasY(
-          (platform.size.width / platformAspectRatio) * this.canvasRatio
-        )
-      );
-    });
+        this.getCanvasY((platform.size.width / platformAspectRatio) * this.canvasRatio),
+      )
+    })
 
     // Draw the player image
-    const playerImage = imagesMap.get(doodleRightImg)!;
+    const playerImage = imagesMap.get(doodleRightImg)!
     // @ts-ignore
-    const playerAspectRatio = playerImage.width / playerImage.height;
+    const playerAspectRatio = playerImage.width / playerImage.height
 
     if (acceleration < 0) {
-      this.ctx.save(); // save the current state
-      this.ctx.scale(-1, 1); // flip context horizontally
+      this.ctx.save() // save the current state
+      this.ctx.scale(-1, 1) // flip context horizontally
       this.ctx.drawImage(
         playerImage,
-        -this.getCanvasX(player.position.x) -
-          this.getCanvasX(player.size.width), // posX needs to be inverted too
+        -this.getCanvasX(player.position.x) - this.getCanvasX(player.size.width), // posX needs to be inverted too
         this.getCanvasY(player.position.y),
         this.getCanvasX(player.size.width),
-        this.getCanvasY(
-          (player.size.width / playerAspectRatio) * this.canvasRatio
-        )
-      );
-      this.ctx.restore(); // restore previous state
+        this.getCanvasY((player.size.width / playerAspectRatio) * this.canvasRatio),
+      )
+      this.ctx.restore() // restore previous state
     } else {
-      this.ctx.save(); // save the current state
-      this.ctx.scale(1, 1); // flip context horizontally
+      this.ctx.save() // save the current state
+      this.ctx.scale(1, 1) // flip context horizontally
       this.ctx.drawImage(
         playerImage,
         this.getCanvasX(player.position.x),
         this.getCanvasY(player.position.y),
         this.getCanvasX(player.size.width),
-        this.getCanvasY(
-          (player.size.width / playerAspectRatio) * this.canvasRatio
-        )
-      );
-      this.ctx.restore(); // restore previous state
+        this.getCanvasY((player.size.width / playerAspectRatio) * this.canvasRatio),
+      )
+      this.ctx.restore() // restore previous state
     }
   }
 
   public destroy() {
-    this.resizeObserver.unobserve(this.canvas);
+    this.resizeObserver.unobserve(this.canvas)
   }
 }
--- src/CanvasImageLoader.ts
+++ src/CanvasImageLoader.ts
@@ -1,10 +1,10 @@
-type ImageSrc = string;
+type ImageSrc = string
 
 export class CanvasImageLoader {
-  private images: Map<ImageSrc, HTMLImageElement>;
+  private images: Map<ImageSrc, HTMLImageElement>
 
   constructor() {
-    this.images = new Map<ImageSrc, HTMLImageElement>();
+    this.images = new Map<ImageSrc, HTMLImageElement>()
   }
 
   // Load a single image
@@ -12,27 +12,27 @@ export class CanvasImageLoader {
     return new Promise((resolve, reject) => {
       if (this.images.has(src)) {
         // Resolve immediately if the image is already loaded
-        resolve();
-        return;
+        resolve()
+        return
       }
 
-      const img = new Image();
+      const img = new Image()
       img.onload = () => {
-        this.images.set(src, img);
-        resolve();
-      };
-      img.onerror = reject;
-      img.src = src;
-    });
+        this.images.set(src, img)
+        resolve()
+      }
+      img.onerror = reject
+      img.src = src
+    })
   }
 
   // Load multiple images
   loadMultipleImages(srcs: ImageSrc[]): Promise<void[]> {
-    return Promise.all(srcs.map((src) => this.loadSingleImage(src)));
+    return Promise.all(srcs.map(src => this.loadSingleImage(src)))
   }
 
   // Retrieve a loaded image
   getImage(src: ImageSrc): HTMLImageElement | undefined {
-    return this.images.get(src);
+    return this.images.get(src)
   }
 }
